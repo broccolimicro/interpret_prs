6ed866c77a76230b2850d320b69e9199b880467f
Ned Bingham <edward.bingham@broccolimicro.io>
2023-07-09 21:57:46 -0400

    adding export function for bubble reshuffling graph

--- interpret_prs/export.cpp
+++ interpret_prs/export.cpp
@@ -7,6 +7,7 @@
 
 #include "export.h"
 #include <interpret_boolean/export.h>
+#include <parse_dot/node_id.h>
 
 parse_prs::production_rule export_production_rule(const prs::production_rule &pr, ucs::variable_set &variables)
 {
@@ -25,3 +26,57 @@ parse_prs::production_rule_set export_production_rule_set(const prs::production_
 		result.rules.push_back(export_production_rule(prs.rules[i], variables));
 	return result;
 }
+
+parse_dot::graph export_bubble(const prs::bubble &bub, const ucs::variable_set &variables)
+{
+	parse_dot::graph graph;
+	graph.type = "digraph";
+	graph.id = "bubble";
+
+	for (size_t i = 0; i < variables.nodes.size(); i++)
+	{
+		graph.statements.push_back(parse_dot::statement());
+		parse_dot::statement &stmt = graph.statements.back();
+		stmt.statement_type = "node";
+		stmt.nodes.push_back(new parse_dot::node_id("V" + to_string(i)));
+		stmt.attributes.attributes.push_back(parse_dot::assignment_list());
+		parse_dot::assignment_list &attr = stmt.attributes.attributes.back();
+		attr.as.push_back(parse_dot::assignment());
+		parse_dot::assignment &a = attr.as.back();
+		a.first = "label";
+		a.second = variables.nodes[i].to_string();
+	}
+
+	for (auto i = bub.net.begin(); i != bub.net.end(); i++)
+	{
+		graph.statements.push_back(parse_dot::statement());
+		parse_dot::statement &stmt = graph.statements.back();
+		stmt.statement_type = "edge";
+		stmt.nodes.push_back(new parse_dot::node_id("V" + to_string(i->first.first)));
+		stmt.nodes.push_back(new parse_dot::node_id("V" + to_string(i->first.second)));
+		if (i->second.first || i->second.second)
+		{
+			stmt.attributes.attributes.push_back(parse_dot::assignment_list());
+			parse_dot::assignment_list &attr = stmt.attributes.attributes.back();
+
+			if (i->second.first)
+			{
+				attr.as.push_back(parse_dot::assignment());
+				parse_dot::assignment &a = attr.as.back();
+				a.first = "style";
+				a.second = "dashed";
+			}
+
+			if (i->second.second)
+			{
+				attr.as.push_back(parse_dot::assignment());
+				parse_dot::assignment &a1 = attr.as.back();
+				a1.first = "arrowhead";
+				a1.second = "odotnormal";
+			}
+		}
+	}
+
+	return graph;
+}
+
--- interpret_prs/export.h
+++ interpret_prs/export.h
@@ -5,18 +5,19 @@
  *      Author: nbingham
  */
 
+#pragma once
+
 #include <common/standard.h>
 
 #include <ucs/variable.h>
 #include <prs/production_rule.h>
+#include <prs/bubble.h>
 
 #include <parse_prs/production_rule.h>
 #include <parse_prs/production_rule_set.h>
-
-#ifndef interpret_prs_export_h
-#define interpret_prs_export_h
+#include <parse_dot/graph.h>
 
 parse_prs::production_rule export_production_rule(const prs::production_rule &pr, ucs::variable_set &variables);
 parse_prs::production_rule_set export_production_rule_set(const prs::production_rule_set &prs, ucs::variable_set &variables);
 
-#endif
+parse_dot::graph export_bubble(const prs::bubble &bub, const ucs::variable_set &variables);
